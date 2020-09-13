f81f6432ecde393a42a2fd6edd69e3cf327d0498
barisenak <mariya1999-7@mail.ru>
2020-09-13 16:14:32 +0300

    with functions

--- index.js
+++ index.js
@@ -11,98 +11,102 @@ const closeBtn = document.querySelector("#closeBtn");
 
 const arr = [];
 
-let date = new Date();
-date = `Created:${date.getDate()}/0${
-  date.getMonth() + 1
-}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+function getDate() {
+  const date = new Date();
+  dateValue = `Created:${date.getDate()}/0${
+    date.getMonth() + 1
+  }/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return dateValue;
+}
+
+function getDesk() {
+  arr.forEach((item) => {
+    desk.innerHTML += `<div class="card"><p class="title">${item.title}</p>
+                  <p class="desc">${item.description}</p>
+                  <p>${item.date}</p>
+                  <button id="delete">Удалить</button>
+                  <button id="edit">Редактировать</button>
+                  </div>`;
+  });
+}
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
 
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   desk.innerHTML = "";
-  arr.push({ title: title.value, description: description.value });
+  getDate();
+  arr.push({
+    title: title.value,
+    description: description.value,
+    date: dateValue,
+  });
   form.reset();
 
-  arr.forEach((item) => {
-    desk.insertAdjacentHTML(
-      "afterbegin",
-      `<div class="card"><p class="title">${item.title}</p>
-        <p class="desc">${item.description}</p>
-        <p>${date}</p>
-        <button id="delete">Удалить</button>
-        <button id="edit">Редактировать</button>
-        </div>`
-    );
-  });
+  getDesk();
   const arrCard = document.querySelectorAll(".card");
   arrCard.forEach((item) => {
     item.addEventListener("mouseenter", (event) => {
-      event.preventDefault();
       item.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
     });
     item.addEventListener("mouseleave", (event) => {
-      event.preventDefault();
       item.style.boxShadow = "none";
     });
   });
 });
 
-desk.addEventListener("click", (event) => {
-  if (event.target.closest("#delete")) {
-    const card = event.target.closest(".card");
-    const titleValue = card.querySelector(".title").textContent;
-    const descriptionValue = card.querySelector(".desc").textContent;
+function doDelete() {
+  const card = event.target.closest(".card");
+  const titleValue = card.querySelector(".title").textContent;
+  const descriptionValue = card.querySelector(".desc").textContent;
 
+  arr.forEach((item, index) => {
+    if (titleValue === item.title && descriptionValue === item.description) {
+      arr.splice(index, 1);
+    }
+  });
+  desk.innerHTML = "";
+  getDesk();
+}
+
+function doEdit() {
+  const card = event.target.closest(".card");
+  let titleValue = card.querySelector(".title").textContent;
+  let descriptionValue = card.querySelector(".desc").textContent;
+  openModal();
+  inputTitle.value = `${titleValue}`;
+  inputDesk.value = `${descriptionValue}`;
+  closeBtn.addEventListener("click", () => {
+    closeModal();
+  });
+  redoBtn.addEventListener("click", () => {
     arr.forEach((item, index) => {
       if (titleValue === item.title && descriptionValue === item.description) {
-        arr.splice(index, 1);
+        getDate();
+        arr.splice(index, 1, {
+          title: inputTitle.value,
+          description: inputDesk.value,
+          date: dateValue,
+        });
+        modal.style.display = "none";
+        desk.innerHTML = "";
+        getDesk();
       }
     });
-    desk.innerHTML = "";
-    arr.forEach((item) => {
-      desk.innerHTML += `<div class="card"><p class="title">${item.title}</p>
-                    <p class="desc">${item.description}</p>
-                    <p>${date}</p>
-                    <button id="delete">Удалить</button>
-                    <button id="edit">Редактировать</button>
-                    </div>`;
-    });
-  }
-});
+  });
+}
 
 desk.addEventListener("click", (event) => {
+  if (event.target.closest("#delete")) {
+    doDelete();
+  }
   if (event.target.closest("#edit")) {
-    const card = event.target.closest(".card");
-    let titleValue = card.querySelector(".title").textContent;
-    let descriptionValue = card.querySelector(".desc").textContent;
-    modal.style.display = "flex";
-    inputTitle.value = `${titleValue}`;
-    inputDesk.value = `${descriptionValue}`;
-    closeBtn.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-    redoBtn.addEventListener("click", () => {
-      arr.forEach((item, index) => {
-        if (
-          titleValue === item.title &&
-          descriptionValue === item.description
-        ) {
-          arr.splice(index, 1, {
-            title: inputTitle.value,
-            description: inputDesk.value,
-          });
-          modal.style.display = "none";
-          desk.innerHTML = "";
-          arr.reverse();
-          arr.forEach((item) => {
-            desk.innerHTML += `<div class="card"><p class="title">${item.title}</p>
-                          <p class="desc">${item.description}</p>
-                          <p>${date}</p>
-                          <button id="delete">Удалить</button>
-                          <button id="edit">Редактировать</button>
-                          </div>`;
-          });
-        }
-      });
-    });
+    doEdit();
   }
 });
